200d02f2c197f3ac52fecbcea7e1bb735b83cc08
Florian Trück <ftrueck@gmail.com>
2026-06-22 11:02:38 +0200

    Register PV power trigger cards

--- drivers/b2500/driver.js
+++ drivers/b2500/driver.js
@@ -3,6 +3,10 @@ const Homey = require('homey');
 
 class B2500Driver extends Homey.Driver {
   async onInit() {
+    this.pvPowerChangedTrigger = this.homey.flow.getDeviceTriggerCard('pv_power_changed');
+    this.pvPowerAboveThresholdTrigger = this.homey.flow.getDeviceTriggerCard('pv_power_above_threshold');
+    this.pvPowerBelowThresholdTrigger = this.homey.flow.getDeviceTriggerCard('pv_power_below_threshold');
+
     this.homey.flow.getActionCard('set_output_power').registerRunListener(async args => args.device.setOutputPower(args.power));
 
     this.homey.flow.getConditionCard('battery_above').registerRunListener(async args => args.device.isCapabilityAbove('measure_battery', args.value));
@@ -23,6 +27,24 @@ class B2500Driver extends Homey.Driver {
     session.setHandler('probe_device', async data => {
       return this.homey.app.probeDevice(data);
     });
+  }
+
+  async triggerPvPowerChanged(device, pvPower) {
+    if (!this.pvPowerChangedTrigger) return;
+    await this.pvPowerChangedTrigger.trigger(device, { pv_power: pvPower }).catch(this.error);
+  }
+
+  async triggerPvPowerThresholds(device, previousPvPower, currentPvPower) {
+    const tokens = { pv_power: currentPvPower };
+    const state = { previousPvPower, currentPvPower };
+
+    if (this.pvPowerAboveThresholdTrigger) {
+      await this.pvPowerAboveThresholdTrigger.trigger(device, tokens, state).catch(this.error);
+    }
+
+    if (this.pvPowerBelowThresholdTrigger) {
+      await this.pvPowerBelowThresholdTrigger.trigger(device, tokens, state).catch(this.error);
+    }
   }
 }
 
